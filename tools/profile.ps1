32a71b4a9ddb8da5b17e8d428cb46e309d5038de
Rene Weber <54496464+r-wbr@users.noreply.github.com>
2024-11-27 08:07:55 +0100

    Updated profile URL

--- tools/profile.ps1
+++ tools/profile.ps1
@@ -1,6 +1,6 @@
 [Console]::OutputEncoding = [Text.Encoding]::UTF8
 
-Import-Module terminal-icons
+Import-Module Terminal-Icons
 
 oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/r-wbr/devcontainers/refs/heads/main/tools/shell.omp.json' | Invoke-Expression
 
