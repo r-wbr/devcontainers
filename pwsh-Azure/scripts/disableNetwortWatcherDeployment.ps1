138bd3b3b03952ad53c362c8dd7f510503d18906
Rene Weber <54496464+r-wbr@users.noreply.github.com>
2024-11-27 07:59:44 +0100

    Removed test files, restructured folders

--- pwsh-Azure/scripts/disableNetwortWatcherDeployment.ps1
+++
@@ -1,5 +0,0 @@
-# Register the "Microsoft.Network" resource provider.
-Register-AzResourceProvider -ProviderNamespace 'Microsoft.Network'
-
-# Register the "DisableNetworkWatcherAutocreation" feature.
-Register-AzProviderFeature -FeatureName 'DisableNetworkWatcherAutocreation' -ProviderNamespace 'Microsoft.Network'
